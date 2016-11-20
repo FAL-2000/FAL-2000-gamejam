5a60c3a9d7bf2ef8a5865db99a74b6707ece4582
vtaipale <valtter.taipale@gmail.com>
2016-11-20 16:43:27 +0200

    Key Pickup
    Red key player can pickup to open doors

---
+++ Assets/Standard Assets/KEY.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KEY : MonoBehaviour {
+
+	public GameObject KeyToRemove;
+
+	public AudioSource GetSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.tag == "Player") {
+
+
+			RayCasterScript MinneAntaa = other.GetComponentInChildren<RayCasterScript>();
+
+			MinneAntaa.ownsKeyRed = true;
+
+			GetSound.Play();
+			
+			
+				
+			KeyToRemove.gameObject.SetActive(false);
+		}
+
+	}
+		
+}
